36417a41d7bbe2fa849aefa0151e34827abac34e
Tanmay Mudholkar <tkmudholkar@gmail.com>
2017-06-02 22:05:52 +0200

    Modified first time setup to open ports 8080 and 8081 as well, and to autoforward port 80 to 8070. Also added print statements to make sure user sees what's going on.

--- firstTimeSetup.sh
+++ firstTimeSetup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+echo "Performing first time setup..."
+echo "Installing packages..."
 sudo yum -y install git bzip2 perl-Switch perl-Env perl-Thread-Queue libXpm-devel libXext-devel mesa-libGLU-devel libXinerama libXi libXft-devel libXrandr libXcursor zsh tk perl-ExtUtils-Embed compat-libstdc++-33 libXmu
+echo "Installed. Opening port 8070, 8080 and 8081..."
 sudo firewall-cmd --zone=public --add-port=8070/tcp --permanent
+echo "...Opened 8070..."
+sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
+echo "...Opened 8080..."
+sudo firewall-cmd --zone=public --add-port=8081/tcp --permanent
+echo "...Opened 8081. Now enabling masquerade..."
+sudo firewall-cmd --zone=public --add-masquerade
+echo "Masquerade enabled. Warning: maybe I am being dense, but this appears to take time to take effect. Now autoforwarding port 80 to 8070..."
+sudo firewall-cmd --zone=public --add-forward-port=port=80:proto=tcp:toport=8070 --permanent
+echo "Autoforwarding set. Now reloading firewalld..."
 sudo firewall-cmd --reload
+echo "Reloaded firewalld. Now creating /dqm-gui and changing permissions..."
 sudo mkdir /dqm-gui
 sudo chown tmudholk:zh /dqm-gui
+echo "All done!"
